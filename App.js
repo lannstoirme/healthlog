1507970343071ff0d3ff188c88586660a31b9c27
Lori Woodward <lannstoirme@Loris-Air.lan>
2020-09-27 20:25:27 +1000

    First commit.

--- App.js
+++ App.js
@@ -1,21 +1,66 @@
+import 'react-native-gesture-handler';
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, TouchableOpacity, Image } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
-export default function App() {
+const MeasureScreen = ({navigation}) => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+      <View><Text>This is the Measure Screen</Text>
+        </View>
+        <TouchableOpacity activeOpacity={0.5} onPress={() => navigation.navigate('Home')} >
+                <Image source={require('./iconbutton.png')} style={styles.FloatingButtonStyle} />
+            </TouchableOpacity>
     </View>
   );
 }
 
+const HomeScreen = ({navigation}) => {
+  return (
+    <View>
+      <View><Text>This is the Home Screen</Text>
+        </View>
+        <TouchableOpacity activeOpacity={0.5} onPress={() => navigation.navigate('Measure')} >
+                <Image source={require('./iconbutton.png')} style={styles.FloatingButtonStyle} />
+            </TouchableOpacity>
+    </View>
+  );
+}
+
+const Stack = createStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+      <Stack.Screen name="Home" component={HomeScreen} />
+      <Stack.Screen name="Measure" component={MeasureScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  text: {
+      fontSize: 20,
+      paddingTop: 10,
+      paddingBottom: 10,
+      textAlign: 'center',
+      fontWeight: 'bold',
   },
+  container:{
+      flex: 1,
+      justifyContent: 'center',
+      backgroundColor: 'green',
+      },
+      FloatingButtonStyle: {
+          resizeMode: 'contain',
+          width: 200,
+          height: 200,
+      },
+  
 });
+
+export default App;
